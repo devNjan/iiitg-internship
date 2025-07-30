9805c149139151118a50e572336cbb6c3ba3198e
devNjan <debanjandebnath05@gmail.com>
2025-07-30 15:12:29 +0530

    Update login.jsx

--- client/src/pages/auth/login.jsx
+++ client/src/pages/auth/login.jsx
@@ -1,23 +1,43 @@
 import CommonForm from "@/components/common/form";
+import { useToast } from "@/components/ui/use-toast";
 import { loginFormControls } from "@/config";
+import { loginUser } from "@/store/auth-slice";
 import { useState } from "react";
+import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 
+const initialState = {
+  email: "",
+  password: "",
+};
+
 function AuthLogin() {
-  const InitialState = {
-    email: "",
-    password: "",
-  };
+  const [formData, setFormData] = useState(initialState);
+  const dispatch = useDispatch();
+  const { toast } = useToast();
 
-  const [formData, setFormData] = useState(InitialState);
+  function onSubmit(event) {
+    event.preventDefault();
 
-  function onSubmit() {}
+    dispatch(loginUser(formData)).then((data) => {
+      if (data?.payload?.success) {
+        toast({
+          title: data?.payload?.message,
+        });
+      } else {
+        toast({
+          title: data?.payload?.message,
+          variant: "destructive",
+        });
+      }
+    });
+  }
 
   return (
     <div className="mx-auto w-full max-w-md space-y-6">
       <div className="text-center">
         <h1 className="text-3xl font-bold tracking-tight text-foreground">
-          Sign in to your Account
+          Sign in to your account
         </h1>
         <p className="mt-2">
           Don't have an account

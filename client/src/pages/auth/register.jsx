3edb432e31df6d44134a09eac1db5dd6aa5b880b
devNjan <debanjandebnath05@gmail.com>
2025-07-30 15:12:47 +0530

    Update register.jsx

--- client/src/pages/auth/register.jsx
+++ client/src/pages/auth/register.jsx
@@ -1,22 +1,47 @@
+import CommonForm from "@/components/common/form";
+import { useToast } from "@/components/ui/use-toast";
 import { registerFormControls } from "@/config";
-import { Link } from "react-router-dom";
+import { registerUser } from "@/store/auth-slice";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+
+const initialState = {
+  userName: "",
+  email: "",
+  password: "",
+};
 
 function AuthRegister() {
-  const InitialState = {
-    userName: "",
-    email: "",
-    password: "",
-  };
+  const [formData, setFormData] = useState(initialState);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { toast } = useToast();
 
-  const [formData, setFormData] = useState(InitialState);
+  function onSubmit(event) {
+    event.preventDefault();
+    dispatch(registerUser(formData)).then((data) => {
+      if (data?.payload?.success) {
+        toast({
+          title: data?.payload?.message,
+        });
+        navigate("/auth/login");
+      } else {
+        toast({
+          title: data?.payload?.message,
+          variant: "destructive",
+        });
+      }
+    });
+  }
 
-  function onSubmit() {}
+  console.log(formData);
 
   return (
     <div className="mx-auto w-full max-w-md space-y-6">
       <div className="text-center">
         <h1 className="text-3xl font-bold tracking-tight text-foreground">
-          Create New Account
+          Create new account
         </h1>
         <p className="mt-2">
           Already have an account
